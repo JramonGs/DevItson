fda8221ec85c234f6d60f1e3d032a67f67c7f8e1
JramonGs <jramongs100@gmail.com>
2025-03-05 10:53:29 -0700

    Implement dark theme toggle functionality and update styles

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/*
 console.log('Hello, world!');
 const name = 'Aaron';
 let apellido = 'Villalobos';
+*/
+
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+
+button.addEventListener("click",()=> {
+    body.classList.toggle("dark-theme");
+})
